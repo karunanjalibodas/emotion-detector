30f8ec5c1c5a2414e09d9bf30561b92ba5d8269f
Bodasu karunanjali <karunanjalibodas13@gmail.com>
2026-02-27 20:11:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
 # Load models
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
@@ -10,22 +11,12 @@ emotion_model = load_model("emotion_model.hdf5", compile=False)
 emotion_labels = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
 
 st.title("😊 Emotion Detector AI")
-st.write("Detect emotions using webcam")
 
-run = st.checkbox("Start Camera")
+class EmotionDetector(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
 
-frame_placeholder = st.empty()
-
-camera = cv2.VideoCapture(0)
-
-if run:
-    while True:
-        ret, frame = camera.read()
-        if not ret:
-            st.error("Camera not working")
-            break
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
         for (x, y, w, h) in faces:
@@ -37,10 +28,10 @@ if run:
             prediction = emotion_model.predict(face, verbose=0)
             emotion = emotion_labels[np.argmax(prediction)]
 
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
-            cv2.putText(frame, emotion, (x,y-10),
+            cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+            cv2.putText(img, emotion, (x,y-10),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
 
-        frame_placeholder.image(frame, channels="BGR")
+        return img
 
-camera.release()
+webrtc_streamer(key="emotion", video_transformer_factory=EmotionDetector)
